e90ace5a3abe7731f21875f2784e4b7f6b9ab6be
YanC Lin <yonalin1@gmail.com>
2018-08-12 05:26:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,13 +22,13 @@ function distanceTravelledInFeet (start,end) {
 function calculatesFarePrice(start, end){
   const distance = distanceTravelledInFeet(start,end);
 
-  if (distance < 400){
-    return 0
-  } else if (distance > 400 && distance <= 2000){
-    return distance * .02
-  } else if (distance > 2000 && distance <=2500){
-    return 25
-  } else {
+  if (distance >= 2500) {
     return 'cannot travel that far';
+  } else if (distance >= 2000) {
+    return 25;
+  } else if (distance >= 400) {
+    return distance * .02;
+  } else {
+    return 0;
   }
 }
