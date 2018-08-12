5dcfed954a22aa38b2502ed679b7b9674edc2422
YanC Lin <yonalin1@gmail.com>
2018-08-12 05:19:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function distanceFromHqInBlocks (distance) {
 function distanceFromHqInFeet (distance) {
   distanceFromHqInBlocks(distance) * 264
 }
+function distanceTravelledInFeet (start,end) {
+  if (start > end){
+    return (start - end)* 264
+  } else {
+    return (end - start)* 264
+  }
+}
+function calculatesFarePrice(start, end){
+  const distance = distanceTravelledInFeet(start,end)
+  if (distance < 400){
+    return 0
+  } else if (distance > 400 && distance <= 2000){
+    return distance * .02
+  } else if (distance > 2000 && distance <= 2500){
+    return 25
+  } else {
+    return 'cannot travel that far';
+  }
+
+}
