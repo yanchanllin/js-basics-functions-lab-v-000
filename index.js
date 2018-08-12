67538cafe551a0808839f0f53b24894572dd4bdd
YanC Lin <yonalin1@gmail.com>
2018-08-12 05:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks (someValue) {
-  //returns the number of blocks given a value
+function distanceFromHqInBlocks (distance) {
+  if (distance > 42){
+    return distance -42
+  } else {
+    return 42 - distance
+  }
 }
- 
+
 function distanceFromHqInFeet (someValue) {
   distanceFromHqInBlocks(someValue);
   // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
